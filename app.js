7c3ddbb422a7847945dc4cbcd93e557a87b7fbb4
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-07-22 10:04:55 -0600

    render employees and write file code added

--- app.js
+++ app.js
@@ -103,20 +103,29 @@ const getEmployees = () => {
                 if (addEmployee === "Yes") {
                     getEmployees();
                 } else {
-                    return false;
+                    
+                    // .then(() => {
+                        fs.writeFile(outputPath, render(employees), err => {
+                            if (err) throw err
+                            console.log("Team members written successfully to output/team.html")
+                        })
+                    // })
                 }
                
             })
+            
+        
         })
+        
 }
 
 getEmployees();
 
 
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-render(employees);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -128,14 +137,4 @@ render(employees);
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
-// {
-//     type: "input",
-//     message: "Enter the Manager's name.",
-//     name: "name"
-// }
